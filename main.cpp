661e6773f4d72e7ec2805c04041196e0df27829c
Lum Naw <lumnaw07@gmail.com>
2016-04-28 11:23:33 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -25,5 +25,30 @@ int main() {
 	cout << endl;
 	list.clearList();
 	list.printList();
+	
+	Stack <int> i_stack;
+    i_stack.push(one);
+    i_stack.push(two);
+    i_stack.push(four);
+    i_stack.showStack();
+    cout<<endl;
+    
+    Node<string> name1("John");
+    Node<string> name2("Adrian");
+    Node<string> name3("James");
+    Node<string> name4("Laura");
+    
+    Stack <string> s_string;
+    s_string.push(name1);
+    s_string.push(name2);
+    s_string.push(name3);
+    s_string.push(name4);
+    s_string.pop();
+    s_string.showStack();
+    s_string.push(name1);
+    s_string.clearStack();
+    s_string.showStack();
+    
+   
 	return 0;
 }
